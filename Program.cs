80998488112e597ada294d947fc00a9d2136a5f3
mariaprylutska <63318704+mariaprylutska@users.noreply.github.com>
2021-05-09 22:57:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Subarray_Division
-{
-    class Program
+class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<int> s, int d, int m) {
+    s.ToArray();
+    int n = s.Count();
+    int result = 0;
+    
+    for(int i = 0; i <= n - m ; i++)
     {
-        static void Main(string[] args)
+        int sum = 0;
+        for(int j = i; j < i + m; j++)
         {
-            Console.WriteLine("Hello World!");
+            sum += s[j];
+        
+        
+         
         }
+        if(sum == d)
+            {
+                result++;    
+            }
+    }
+    return result;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(dm[0]);
+
+        int m = Convert.ToInt32(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
