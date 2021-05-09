5a2f0283a68505c310e7861d3aff0e59d687b060
mariaprylutska <63318704+mariaprylutska@users.noreply.github.com>
2021-05-09 22:54:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Subarray_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
